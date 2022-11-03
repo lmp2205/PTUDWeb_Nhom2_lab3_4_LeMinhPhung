4b280daf91d64eeab546342797f6c77f062e6395
Minh Khang <lephungcrazy@gmail.com>
2022-11-03 22:24:37 +0700

    Tao trang them moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,52 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="createContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {},
+            message: "",
+        };
+    },
+    methods: {
+        async getContact(id) {
+            try {
+                this.contact = await ContactService.get(id);
+            } catch (error) {
+                console.log(error);
+                // Chuyển sang trang NotFound đồng thời giữ cho URL không đổi
+                this.$router.push({
+                    name: "notfound",
+                    params: {
+                        pathMatch: this.$route.path.split("/").slice(1)
+                    },
+                    query: this.$route.query,
+                    hash: this.$route.hash,
+                });
+            }
+        },
+        async createContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được thêm thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+    created() {
+        this.message = "";
+    },
+};
+</script>
+    
